6a3499464933309252e01a7537c6404e58868c42
FranCICSo <fraromero@frba.utn.edu.ar>
2022-08-24 13:20:44 -0300

    Nombrado de paquetes y de artefacto, y plugin maven para levantar jar

--- backend/src/main/java/frba/utn/tacs/matches/organizer/MatchesOrganizerApp.java
+++ backend/src/main/java/frba/utn/tacs/matches/organizer/MatchesOrganizerApp.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package frba.utn.tacs.matches.organizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class MatchesOrganizerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MatchesOrganizerApp.class, args);
 	}
 
 	@GetMapping("/hello")
--- backend/src/test/java/frba/utn/tacs/matches/organizer/MatchesOrganizerAppTest.java
+++ backend/src/test/java/frba/utn/tacs/matches/organizer/MatchesOrganizerAppTest.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package frba.utn.tacs.matches.organizer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MatchesOrganizerAppTest {
 
 	@Test
 	void contextLoads() {
