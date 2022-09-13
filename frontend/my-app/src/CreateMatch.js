42e8de3878faa6b10eb7d0426b6f169a23bbf281
Leonel Bazan <leo.bazan.x@gmail.com>
2022-09-13 20:17:31 -0300

    FE - CreateMatchComponent: Add basic validation in form

--- frontend/my-app/src/CreateMatch.js
+++ frontend/my-app/src/CreateMatch.js
@@ -7,7 +7,6 @@ import AddIcon from '@mui/icons-material/Add';
 import { Box } from '@mui/system';
 import Button from '@mui/material/Button';
 import dayjs from 'dayjs';
-import 'dayjs/locale/es';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Grid from '@mui/material/Grid';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -16,14 +15,13 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import TextField from '@mui/material/TextField';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 
-
 export default function CreateMatch() {
-
-  const [value, setValue] = React.useState(dayjs());
-  const [_locale, _setLocale] = React.useState('es');
+  const [dateTime, setDateTime] = React.useState(nowPlus30Min());
+  const [minTime, setMinTime] = React.useState(nowPlus30Min());
+  const today = dayjs();
 
   return(
-    <Box component="form" noValidate autoComplete="off">
+    <Box component="form" autoComplete="off">
     <div>
       <FormSpace/>
       <RequiredTextField adornment={<AbcIcon/>} id={"name"} defaultValue={"Nombre del Partido"}/>
@@ -33,29 +31,36 @@ export default function CreateMatch() {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
           label="Fecha"
-          value={value}
+          value={dateTime}
           onChange={(newValue) => {
-            setValue(newValue);
-          }}
+            setDateTime(newValue);
 
+            var isAfterToday = dayjs(newValue).isAfter(dayjs(), 'day');
+            if (isAfterToday) {setMinTime(dayjs().minute(0).hour(0).second(0));}
+            else{setMinTime(nowPlus30Min());}
+          }
+        }
           renderInput={(params) => <TextField {...params} id={"date"} sx={{ width: 0.5}} />}
           inputFormat="YYYY-MM-DD"
+          minDate={today}
         />
 
         <TimePicker
           label="Hora"
-          value={value}
+          value={dateTime}
           onChange={(newValue) => {
-            setValue(newValue);
+            setDateTime(newValue);
           }}
-          inputFormat="HH:MM:00"
+          inputFormat="HH:mm:00"
           renderInput={(params) => <TextField {...params} id={"time"} sx={{ width: 0.5}} />}
+          minTime={minTime}
         />
       </LocalizationProvider>
       <FormSpace/>
 
       <Grid container justifyContent="flex-end">
-        <Button variant="contained" startIcon={<AddIcon/>} onClick={() => {postCreateMatch();}}>
+        <Button variant="contained" startIcon={<AddIcon/>} onClick={() => {
+          if(validateForm(dateTime)){postCreateMatch();}}}>
           Crear Partido
         </Button>
       </Grid>
@@ -85,6 +90,26 @@ class RequiredTextField extends React.Component{
   />)
   }
 }
+
+function nowPlus30Min() {
+  return dayjs().add(30, 'minute');
+}
+
+function validateForm(dateTime) {
+  if (document.getElementById("name").value.trim() == "") {
+      alert("Campo nombre es requerido");
+      return false;
+  }
+  if (document.getElementById("location").value.trim() == "") {
+    alert("Campo Ubicacion es requerido");
+    return false;
+  }
+  if (dateTime.isBefore(dayjs())) {
+    alert("Fecha y hora deben ser posterior al momento actual");
+    return false;
+  }
+  return true;
+};
 
 async function postCreateMatch() {
   // TODO: Externalizar a variables de configuracion
