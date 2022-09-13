876071c92524e4118155dc384495b88d450319c5
Leonel Bazan <leo.bazan.x@gmail.com>
2022-09-13 13:39:46 -0300

    BE - MatchController: CrossOrigin Allow All

--- backend/src/main/java/matches/organizer/controller/MatchController.java
+++ backend/src/main/java/matches/organizer/controller/MatchController.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @EnableWebMvc
 public class MatchController {
