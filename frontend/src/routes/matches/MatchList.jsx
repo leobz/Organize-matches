7c14ecfff72d1e9d0ef93742bd306481bc583629
Leonel Bazán <lbazan@gmail.com>
2022-09-27 02:02:33 -0300

    FE - Remane folder Matches

--- frontend/src/routes/matches/MatchList.jsx
+++ frontend/src/routes/matches/MatchList.jsx
@@ -8,7 +8,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 
 export const loader = async () => {
-    return fetch("http://localhost:8081/matches").then(resp => {
+    return fetch("http://localhost:8081/matches",  {
+        method: 'GET',
+        credentials: 'include'
+      }).then(resp => {
         return resp.json()
     });
 }
