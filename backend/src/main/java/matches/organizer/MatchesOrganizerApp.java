dca7ddab59ab240a21d6ac5e679f2461140bd148
Leonel Bazán <leo.bazan.x@gmail.com>
2022-10-15 21:36:10 -0300

    TelegramBot: Initalize + Basic /list command

--- backend/src/main/java/matches/organizer/MatchesOrganizerApp.java
+++ backend/src/main/java/matches/organizer/MatchesOrganizerApp.java
@@ -1,12 +1,30 @@
 package matches.organizer;
 
+import matches.organizer.service.BotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Component
 public class MatchesOrganizerApp {
+
+	@Autowired
+	private BotService bot;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchesOrganizerApp.class, args);
+	}
+
+	@PostConstruct
+	public void initializeBot() throws TelegramApiException {
+		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+		botsApi.registerBot(bot);
 	}
 }
